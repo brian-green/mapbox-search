1dbe56977aa82ce662c1b12bdb313218e67ee8f0
Brian Green <dear.briangreen@gmail.com>
2020-12-01 22:33:45 -0800

    Added map

--- src/Map.js
+++ src/Map.js
@@ -1,9 +1,23 @@
 import React, { Component } from 'react';
+import mapbox from 'mapbox-gl'
+import './Map.css'
 
 class Map extends Component {
+
+  //calls this in the lifecycle after id=map is loaded below
+  componentDidMount() {
+    mapbox.accessToken = 'pk.eyJ1IjoiYnJpaWlhbiIsImEiOiJja2k0MHA3MWowY3R2Mnhtc25ycHE5bmcyIn0.OY0hKbYzgY24P52Lz3SHFA';
+
+    var map = new mapbox.Map({
+      container: 'map',
+      style: 'mapbox://styles/mapbox/streets-v11'
+      });
+
+  }
+
   render () {
     return (
-      <div className="App">
+      <div id="map">
         Map goes here
       </div>
     );
